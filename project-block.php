eb94240fc7bdaa3002dbf93047c28d22dd815486
blufox21 <lunevm@list.ru>
2019-07-04 18:06:40 +0300

    Update project-block.php

--- project-block.php
+++ project-block.php
@@ -1,21 +1,20 @@
-<?php for ($i=0;$i<5;$i++){?>
-    <div class="proj">
-			<div class="rating">
-				<div class="likes">
-					<span>20 L</span>
-					<img src="res/images/arrow.png">
-				</div>
 
-				<div class="dislikes">
-					<span>10 DL</span>
-					<img src="res/images/arrowD.png">
-				</div>
-			</div>
-			<div>
-				<img src="res/images/placeholder.png" class="project-thumbnail">
-			</div>
-			<div>
-				<h1 class="project-name">Lorem Ipsum Dolor</h1>
-			</div>
+<div class="proj" href="project-main-page.php">
+	<div class="rating">
+		<div class="likes">
+			<span>20 L</span>
+			<img src="res/images/arrow.png">
 		</div>
-<?php }?>
+
+		<div class="dislikes">
+			<span>10 DL</span>
+			<img src="res/images/arrowD.png">
+		</div>
+	</div>
+	<div>
+		<img src="res/images/placeholder.png" class="project-thumbnail">
+	</div>
+	<div>
+		<h1 class="project-name">Lorem Ipsum Dolor</h1>
+	</div>
+</div>
