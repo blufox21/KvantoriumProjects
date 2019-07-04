51d4c093fd76b3a2ba5db79e0a70ffc8a0c7019f
blufox21 <lunevm@list.ru>
2019-07-04 18:09:14 +0300

    Add files via upload

---
+++ project-main-page.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Главная</title>
+        <link rel="stylesheet" href="styles/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Ubuntu:700&display=swap&subset=cyrillic" rel="stylesheet">  
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <header>
+            <div class="header">
+                <div class="logo">
+                    <img src="res/images/Logo.png">
+                </div>
+
+                <div class="search">
+                        <img src="res/images/magnif.png" href=#>
+                        <img src="res/images/sign.png" href=#>
+                </div>
+            </div>
+
+            <div class="nav">
+                <a href="#">БИО</a>
+                <a href="#">IT</a>
+                <a href="#">РОБО</a>
+                <a href="#">ЭНЕРДЖИ</a>
+                <a href="#">ПРОМДИЗАЙН</a>
+                <a href="#">ХАЙКТЕК</a>
+            </div>
+        </header>
+        <div class="main">
+            
+        </div>
+        <footer>
+            <div class="phone">
+                <img src="res/images/phone.png" alt="" class="phone-logo">
+                <span class="phone-number">+7 (863) 238-15-68,  +7 (863) 251-22-81</span>
+            </div>
+            <div class="mail">
+                <img src="res/images/mail.png" alt="" class="mail-logo">
+                <span class="email">info@kvantorium-don.ru</span>
+            </div>
+        </footer>
+    </body>
+</html>
