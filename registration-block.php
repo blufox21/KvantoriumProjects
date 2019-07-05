6c04284122f7d501b796f016e9be120ea8344672
blufox21 <lunevm@list.ru>
2019-07-05 14:55:59 +0300

    Add files via upload

---
+++ registration-block.php
@@ -0,0 +1,15 @@
+<div class="popup">
+    <div class="signinup">
+        <a>Регистрация</a>
+        /
+        <a>войти</a>
+    </div>
+
+    <div class="reg">
+        segsshsdshshshesesshessheshshsehs
+    </div>
+
+    <div class="login">
+
+    </div>
+</div>
