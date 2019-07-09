0c69c5e0dcae7382843e174e170251c5029cabc2
blufox21 <lunevm@list.ru>
2019-07-09 18:03:52 +0300

    Update registration-block.php

--- registration-block.php
+++ registration-block.php
@@ -1,15 +1,32 @@
 <div class="popup">
     <div class="signinup">
-        <a>Регистрация</a>
+        <a onclick="document.getElementById('reg').style.display = 'block';
+                    document.getElementById('login').style.display = 'none';">Регистрация</a>
         /
-        <a>войти</a>
+        <a onclick="document.getElementById('reg').style.display = 'none';
+                    document.getElementById('login').style.display = 'block';">войти</a>
     </div>
 
-    <div class="reg">
-        segsshsdshshshesesshessheshshsehs
+    <div id="reg">
+        <div class="field">
+            <span>Введите желаемый ник:</span>
+            <input type="text" placeholder="Username">
+        </div>
+        <div class="field">
+            <span>Введите пароль:</span>
+            <input type="text" placeholder="Password">
+        </div>
+        <div class="field">
+            <span>Введите ваше ФИО (полностью):</span>
+            <input type="text">
+        </div>
+        <div class="field">
+            <span>Введите ваш Email:</span>
+            <input type="text" placeholder="example@mail.ru">
+        </div>
     </div>
 
-    <div class="login">
+    <div id="login">
 
     </div>
 </div>
