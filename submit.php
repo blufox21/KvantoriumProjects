5049f60b4bb84c6e87f20031ec8e42c1fc1d23f8
blufox21 <lunevm@list.ru>
2019-07-18 18:40:52 +0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,30 +1,49 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-    $username = $_POST['username'];
-    $pass = $_POST['password'];
-    $name = $_POST['fio'];
-    $mail = $_POST['email'];
+<html>
+    <head></head>
+    <body>
+        <div>
+            <?php 
+                ob_start();
+                header('Content-Type: text/html; charset=utf-8');
+                $username = $_POST['username'];
+                $pass = md5($_POST['password']);
+                $name = $_POST['fio'];
+                $mail = $_POST['email'];
 
-    $host = "localhost";
-    $dbUser = "root";
-    $dbPass = "";
-    $dbName = "Main";
-    if(!empty($username) && !empty($pass) && !empty($name) && !empty($mail)){
-        $conn = new mysqli($host, $dbUser, $dbPass, $dbName);
-        $conn->set_charset('utf8');
-        if (mysqli_connect_error()) {
-            printf('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-        } 
-        else {
-            $query = "INSERT INTO users VALUES ('" . $username . "','" . $pass . "','" . $name . "','" . $mail . "')";
-            printf($query);
-            printf($conn->query($query));
-        }
-    }
-    else{
-        echo("Заполните все поля");
-        die();
-    }
+                $host = "localhost";
+                $dbUser = "root";
+                $dbPass = "";
+                $dbName = "Main";
+                $conn = new mysqli($host, $dbUser, $dbPass, $dbName);
+                $conn->set_charset('utf8');
+                
+                if(!empty($username) && !empty($pass) && !empty($name) && !empty($mail)){
+                    if (mysqli_connect_error()) {
+                        printf('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+                    } 
+                    else {
+                        $checkQuery = "SELECT * FROM users WHERE username='" . $username . "' OR email='" . $mail . "'";
+                        $result = $conn->query($checkQuery);
+                        $query = "INSERT INTO users VALUES ('" . $username . "','" . $pass . "','" . $name . "','" . $mail . "')";
+                        if($result->num_rows  > 0){
+                            echo('<div> </div>');
+                            header('refresh: 10; url=index.php');
+                        }
+                        else{
+                            $conn->query($query);
+                            header('refresh: 10; url=index.php');
+                        }
+                    }
+                }
+                else{
+                    echo("Заполните все поля");
+                    die();
+                }
 
-    mysqli_close($conn);
-?>
+                mysqli_close($conn);
+            ?>
+
+            <p>вы вернётесь на начальную через 10сек</p>
+        </div>
+    </body>
+</html>
