f5d12818ee0700d096d6b06e0159cfe03dddf180
blufox21 <lunevm@list.ru>
2019-09-28 19:47:25 +0300

    Update project-block.php

--- project-block.php
+++ project-block.php
@@ -1,4 +1,4 @@
-
+<!--
 <div class="proj" href="project-main-page.php">
 	<div class="rating">
 		<div class="likes">
@@ -16,5 +16,25 @@
 	</div>
 	<div>
 		<h1 class="project-name">Lorem Ipsum Dolor</h1>
+	</div>
+</div>
+-->
+
+<div class="proj-card" href="project-main-page.php">
+	<img src="res/images/placeholder.png" alt="res/images/placeholder.png" class="proj-image">
+	<div class="info">
+		<h1 class="project-name">Lorem Ipsum Dolor</h1>
+		<p class="project-descriptor">Lorem ipsum dolor...</p>
+	</div>
+	<div class="rating">
+		<div class="likes">
+			<span>20 L</span>
+			<img src="res/images/arrow.png">
+		</div>
+
+		<div class="dislikes">
+			<span>10 DL</span>
+			<img src="res/images/arrowD.png">
+		</div>
 	</div>
 </div>
