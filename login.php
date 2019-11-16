75e278619ec85359a3a4b9a10aefb238bb93061b
blufox21 <lunevm@list.ru>
2019-11-16 19:57:45 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,46 +1,47 @@
 <?php
 //require_once "config.php";
 
-// Check if the user is already logged in, if yes then redirect him to the main page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: index.php");
-    exit;
-}
-
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    //check field emptiness
-    if(!empty($_POST["username"])){
-        $username = trim($_POST["username"]);
-    }
-    else{
-        echo("Заполните все поля");
-        die();
-    }
 
-    if(!empty($_POST["password"])){
-        $password = md5(trim($_POST["password"]));
-    }
-    else{
-        echo("Заполните все поля");
-        die();
-    }
+    if($_SESSION["loggedin"] === false){
+        //check field emptiness
+        if(!empty($_POST["username"])){
+            $username = trim($_POST["username"]);
+        }
+        else{
+            echo("Заполните все поля");
+            die();
+        }
 
-    //sql query
-    $sql = "SELECT * FROM users WHERE username='".$username."' AND pass='".$password."'";
-    //execute query
-    $res = $conn->query($sql);
+        if(!empty($_POST["password"])){
+            $password = md5(trim($_POST["password"]));
+        }
+        else{
+            echo("Заполните все поля");
+            die();
+        }
 
-    if($res->num_rows > 0){
+        //sql query
+        $sql = "SELECT * FROM users WHERE username='".$username."' AND pass='".$password."'";
+        //execute query
+        $res = $conn->query($sql);
 
-        $_SESSION["loggedin"] = true;
-        $_SESSION["user"] = $username;
+        if($res->num_rows > 0){
 
-        //redirect back to main page
-        <script>window.location.href= "index.php";</script>
-        //header("location: index.php");
+            $_SESSION["loggedin"] = true;
+            $_SESSION["user"] = $username;
+
+            //redirect back to main page
+            echo('<script>window.location.href="index.php";</script>');
+            //header("location: index.php");
+        }else{
+            echo("неверный пароль или ник");
+        }
     }else{
-        echo("неверный пароль или ник");
+        $_SESSION["loggedin"] = false;
+        $_SESSION["user"] = "";
+        echo('<script>window.location.href="index.php";</script>');
     }
 }
 ?>
